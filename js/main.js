86e3b62febb570ba0b7398fc99d88e22c00ed758
Silvia Ribeiro <silviaribeiro.1994@gmail.com>
2020-02-05 07:17:23 +0000

    Add transition animation between steps

--- js/main.js
+++ js/main.js
@@ -18,5 +18,51 @@ function nifLive() {
 // LOGO COLOR - LIVE PREVIEW
 function colorLive() {
     let logocolor = document.getElementById("color").value;
-        document.getElementById("logo-color").style.backgroundColor = "#" + logocolor;
+    document.getElementById("logo-color").style.backgroundColor = "#" + logocolor;
 }
+
+//Animations
+function step1end() {
+    //change dot status
+    document.getElementById("step1num").className = "step-number-off";
+    document.getElementById("step2num").className = "step-number";
+    //change title status
+    document.getElementById("step1title").className = "h2-off";
+    document.getElementById("step2title").className = "h2-on";
+    //fade-out step1
+    document.getElementById("step1content").classList.remove("fade-in");
+    document.getElementById("step1content").classList.add("fade-out");
+    setTimeout(function () { document.getElementById("step1content").style.display = "none" }, 1000);
+    //fade-in step2
+    document.getElementById("step2content").classList.remove("fade-out");
+    setTimeout(function () {
+        document.getElementById("step2content").style.display = "block";
+    }, 1000);
+    setTimeout(function () {
+        document.getElementById("step2content").classList.add("fade-in");
+    }, 1000);
+}
+
+function step2end() {
+    //change dot status
+    document.getElementById("step1num").className = "step-number";
+    document.getElementById("step2num").className = "step-number-off";
+    //change title status
+    document.getElementById("step1title").className = "h2-on";
+    document.getElementById("step2title").className = "h2-off";
+    //fade-out step2
+    document.getElementById("step2content").classList.remove("fade-in");
+    document.getElementById("step2content").classList.add("fade-out");
+    setTimeout(function () { document.getElementById("step2content").style.display = "none" }, 1000);
+    //fade-in step1
+    document.getElementById("step1content").classList.remove("fade-out");
+    setTimeout(function () {
+        document.getElementById("step1content").style.display = "block";
+    }, 1000);
+    setTimeout(function () {
+        document.getElementById("step1content").classList.add("fade-in");
+    }, 1000);
+}
+
+
+
